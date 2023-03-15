750a8e2d40c40dd31a2c8a6de3e1c44c2318a823
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-03-15 22:43:49 +0700

    setting up the form

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,28 @@
-<div>
-    Tes Register
-</div>
+<form wire:submit.prevent="register">
+    <div>
+        <label for="name">Name</label>
+        <input wire:model="name" type="text" name="name" id="name">
+    </div>
+
+    <div>
+        <label for="email">Email</label>
+        <input wire:model="email" type="text" name="email" id="email">
+    </div>
+
+
+    <div>
+        <label for="password">Password</label>
+        <input wire:model="password" type="password" name="password" id="password">
+    </div>
+
+
+    <div>
+        <label for="passwordConfirmation">Password Confirmation</label>
+        <input wire:model="passwordConfirmation" type="password" name="passwordConfirmation" id="passwordConfirmation">
+    </div>
+
+
+    <div>
+        <input type="submit" value="Register">
+    </div>
+</form>
