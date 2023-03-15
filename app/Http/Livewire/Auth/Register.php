782576762ef4f1bc874247aecd964c67525fe08c
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-03-15 23:01:26 +0700

    adding validation

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -15,12 +15,21 @@ class Register extends Component
 
     public function register()
     {
-        // return "tes";
-        User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => Hash::make($this->password)
+        // livewire tidak mengambil data dari request karena dia langsung mengambil data dari var yang sudah dihubungkan dengan wire:model pada inputan
+        $data = $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|same:passwordConfirmation',
         ]);
+
+
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return redirect('/');
     }
 
     public function render()
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -2,17 +2,26 @@
     <div>
         <label for="name">Name</label>
         <input wire:model="name" type="text" name="name" id="name">
+        @error('name')
+            <span>{{ $message }}</span>
+        @enderror
     </div>
 
     <div>
         <label for="email">Email</label>
         <input wire:model="email" type="text" name="email" id="email">
+        @error('email')
+            <span>{{ $message }}</span>
+        @enderror
     </div>
 
 
     <div>
         <label for="password">Password</label>
         <input wire:model="password" type="password" name="password" id="password">
+        @error('password')
+            <span>{{ $message }}</span>
+        @enderror
     </div>
 
 
